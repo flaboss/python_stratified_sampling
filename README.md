c8e74193aa991f989873c1ed229c95dff26076c3
flavio.bossolan@cea.com.br <flavio.bossolan@cea.com.br>
2019-06-29 18:34:11 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
 # python_stratified_sampling
 This is a helper python module to be used along side pandas. It creates stratified sampling based on given strata.
+
+## Documentation
+
+### stratified_sample(df, strata, size=None, seed=None)
+It samples data from a pandas dataframe using strata. These functions use
+    proportionate stratification:
+    n1 = (N1/N) * n
+    where:
+        - n1 is the sample size of stratum 1
+        - N1 is the population size of stratum 1
+        - N is the total population size
+        - n is the sampling size
+
+    Parameters
+    ----------
+    :df: pandas dataframe from which data will be sampled.
+    :strata: list containing columns that will be used in the stratified sampling.
+    :size: sampling size. If not informed, a sampling size will be calculated
+        using Cochran adjusted sampling formula:
+        cochran_n = (Z**2 * p * q) /e**2
+
+        where:
+            - Z is the z-value. In this case we use 1.96 representing 95%
+            - p is the estimated proportion of the population which has an
+                attribute. In this case we use 0.5
+            - q is 1-p
+            - e is the margin of error
+
+        This formula is adjusted as follows:
+        adjusted_cochran = cochran_n / 1+((cochran_n -1)/N)
+
+        where:
+            - cochran_n = result of the previous formula
+            - N is the population size
+
+    Returns
+    -------
+    A sampled pandas dataframe based in a set of strata.
+
+    Examples
+    --------
+    >> df.head()
+    	id  sex age city 
+    0	123 M   20  XYZ
+    1	456 M   25  XYZ
+    2	789 M   21  YZX
+    3	987 F   40  ZXY
+    4	654 M   45  ZXY
+    ...
+    # This returns a sample stratified by sex and city containing 30% of the size of
+    # the original data
+    >> stratified = stratified_sample(df=df, strata=['sex', 'city'], size=0.3)
+
+    Requirements
+    ------------
+    - pandas
+    - numpy
+
+### stratified_sample_report(df, strata, size=None)
+    Generates a dataframe reporting the counts in each stratum and the counts
+    for the final sampled dataframe.
+
+    Parameters
+    ----------
+    :df: pandas dataframe from which data will be sampled.
+    :strata: list containing columns that will be used in the stratified sampling.
+    :size: sampling size. If not informed, a sampling size will be calculated
+        using Cochran adjusted sampling formula:
+        cochran_n = (Z**2 * p * q) /e**2
+
+        where:
+            - Z is the z-value. In this case we use 1.96 representing 95%
+            - p is the estimated proportion of the population which has an
+                attribute. In this case we use 0.5
+            - q is 1-p
+            - e is the margin of error
+
+        This formula is adjusted as follows:
+        adjusted_cochran = cochran_n / 1+((cochran_n -1)/N)
+
+        where:
+            - cochran_n = result of the previous formula
+            - N is the population size
+
+    Returns
+    -------
+    A dataframe reporting the counts in each stratum and the counts
+    for the final sampled dataframe.
+
+### __smpl_size(population, size)
+    An internal function to compute the sample size. If not informed, a sampling size will be calculated using Cochran adjusted sampling formula:
+        cochran_n = (Z**2 * p * q) /e**2
+
+        where:
+            - Z is the z-value. In this case we use 1.96 representing 95%
+            - p is the estimated proportion of the population which has an
+                attribute. In this case we use 0.5
+            - q is 1-p
+            - e is the margin of error
+
+        This formula is adjusted as follows:
+        adjusted_cochran = cochran_n / 1+((cochran_n -1)/N)
+
+        where:
+            - cochran_n = result of the previous formula
+            - N is the population size
+    Parameters
+    ----------
+        :population: population size
+        :size: sample size (default = None)
+    Returns
+    -------
+    Calculated sample size to be used in the functions:
+        - stratified_sample
+        - stratified_sample_report
+
---
+++ stratifiedSample.py
@@ -0,0 +1,183 @@
+'''
+This module contains functions that computes stratified sampling of pandas dataframes.
+'''
+# Required libraries
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+
+# Functions
+def stratified_sample(df, strata, size=None, seed=None):
+    '''
+    It samples data from a pandas dataframe using strata. These functions use
+    proportionate stratification:
+    n1 = (N1/N) * n
+    where:
+        - n1 is the sample size of stratum 1
+        - N1 is the population size of stratum 1
+        - N is the total population size
+        - n is the sampling size
+
+    Parameters
+    ----------
+    :df: pandas dataframe from which data will be sampled.
+    :strata: list containing columns that will be used in the stratified sampling.
+    :size: sampling size. If not informed, a sampling size will be calculated
+        using Cochran adjusted sampling formula:
+        cochran_n = (Z**2 * p * q) /e**2
+
+        where:
+            - Z is the z-value. In this case we use 1.96 representing 95%
+            - p is the estimated proportion of the population which has an
+                attribute. In this case we use 0.5
+            - q is 1-p
+            - e is the margin of error
+
+        This formula is adjusted as follows:
+        adjusted_cochran = cochran_n / 1+((cochran_n -1)/N)
+
+        where:
+            - cochran_n = result of the previous formula
+            - N is the population size
+
+    Returns
+    -------
+    A sampled pandas dataframe based in a set of strata.
+
+    Examples
+    --------
+    >> df.head()
+    	id  sex age city 
+    0	123 M   20  XYZ
+    1	456 M   25  XYZ
+    2	789 M   21  YZX
+    3	987 F   40  ZXY
+    4	654 M   45  ZXY
+    ...
+    # This returns a sample stratified by sex and city containing 30% of the size of
+    # the original data
+    >> stratified = stratified_sample(df=df, strata=['sex', 'city'], size=0.3)
+
+    Requirements
+    ------------
+    - pandas
+    - numpy
+    '''
+    population = len(df)
+    size = __smpl_size(population, size)
+    tmp = df[strata]
+    tmp['size'] = 1
+    tmp_grpd = tmp.groupby(strata).count().reset_index()
+    tmp_grpd['samp_size'] = round(size/population * tmp_grpd['size']).astype(int)
+
+    # controlling variable to create the dataframe or append to it
+    first = True 
+    for i in range(len(tmp_grpd)):
+        # query generator for each iteration
+        qry=''
+        for s in range(len(strata)):
+            stratum = strata[s]
+            value = tmp_grpd.iloc[i][stratum]
+            n = tmp_grpd.iloc[i]['samp_size']
+
+            if type(value) == str:
+                value = "'" + str(value) + "'"
+            
+            if s != len(strata)-1:
+                qry = qry + stratum + ' == ' + str(value) +' & '
+            else:
+                qry = qry + stratum + ' == ' + str(value)
+        
+        # final dataframe
+        if first:
+            stratified_df = df.query(qry).sample(n=n, random_state=seed).reset_index(drop=True)
+            first = False
+        else:
+            tmp_df = df.query(qry).sample(n=n, random_state=seed).reset_index(drop=True)
+            stratified_df = stratified_df.append(tmp_df, ignore_index=True)
+    
+    return stratified_df
+
+
+
+def stratified_sample_report(df, strata, size=None):
+    '''
+    Generates a dataframe reporting the counts in each stratum and the counts
+    for the final sampled dataframe.
+
+    Parameters
+    ----------
+    :df: pandas dataframe from which data will be sampled.
+    :strata: list containing columns that will be used in the stratified sampling.
+    :size: sampling size. If not informed, a sampling size will be calculated
+        using Cochran adjusted sampling formula:
+        cochran_n = (Z**2 * p * q) /e**2
+
+        where:
+            - Z is the z-value. In this case we use 1.96 representing 95%
+            - p is the estimated proportion of the population which has an
+                attribute. In this case we use 0.5
+            - q is 1-p
+            - e is the margin of error
+
+        This formula is adjusted as follows:
+        adjusted_cochran = cochran_n / 1+((cochran_n -1)/N)
+
+        where:
+            - cochran_n = result of the previous formula
+            - N is the population size
+
+    Returns
+    -------
+    A dataframe reporting the counts in each stratum and the counts
+    for the final sampled dataframe.
+    '''
+    population = len(df)
+    size = __smpl_size(population, size)
+    tmp = df[strata]
+    tmp['size'] = 1
+    tmp_grpd = tmp.groupby(strata).count().reset_index()
+    tmp_grpd['samp_size'] = round(size/population * tmp_grpd['size']).astype(int)
+    return tmp_grpd
+
+
+def __smpl_size(population, size):
+    '''
+    A function to compute the sample size. If not informed, a sampling 
+    size will be calculated using Cochran adjusted sampling formula:
+        cochran_n = (Z**2 * p * q) /e**2
+
+        where:
+            - Z is the z-value. In this case we use 1.96 representing 95%
+            - p is the estimated proportion of the population which has an
+                attribute. In this case we use 0.5
+            - q is 1-p
+            - e is the margin of error
+
+        This formula is adjusted as follows:
+        adjusted_cochran = cochran_n / 1+((cochran_n -1)/N)
+
+        where:
+            - cochran_n = result of the previous formula
+            - N is the population size
+    Parameters
+    ----------
+        :population: population size
+        :size: sample size (default = None)
+    Returns
+    -------
+    Calculated sample size to be used in the functions:
+        - stratified_sample
+        - stratified_sample_report
+    '''
+    if size is None:
+        cochran_n = round(((1.96)**2 * 0.5 * 0.5)/ 0.02**2)
+        n = round(cochran_n/(1+((cochran_n -1) /population)))
+    elif size >= 0 and size < 1:
+        n = round(population * size)
+    elif size < 0:
+        raise ValueError('Parameter "size" must be an integer or a proportion between 0 and 0.99.')
+    elif size >= 1:
+        n = size
+    return n
